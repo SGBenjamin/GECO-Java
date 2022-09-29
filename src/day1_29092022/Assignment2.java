eaea40b16e02fbbed04b1b071b08fe68b1b6b34d
Tat Sheng Benjamin Chan <Benjamin Chan>
2022-09-29 13:39:58 +0800

    Assignment Submission

---
+++ src/day1_29092022/Assignment2.java
@@ -0,0 +1,13 @@
+package day1_29092022;
+
+public class Assignment2 {
+    String[] arr = {"abc","def","ghi","klm","NOP"};
+
+    public static void main(String[] args) {
+        Assignment2 assignment2 = new Assignment2();
+
+        for(int i =0; i<assignment2.arr.length; i++){
+            System.out.println(assignment2.arr[i]);
+        }
+    }
+}
